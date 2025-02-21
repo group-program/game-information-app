5aff1c1300cf14fa697a6c99f010b72da483e25f
Mintostar <kazukihokama@gmail.com>
2025-02-21 19:45:49 +0900

    Add: 記事カードコンポーネントを追加 - ダミーデータを使用 - リンクは未実装 - 文字列が長い場合、スタイルが崩れる
    Add: ホームページに記事一覧を追加
    
    Refactor: ホームページの関数宣言を関数式に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
-import React from "react";
+import ArticleCard from "./components/ArticleCard";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
     <h1>apex最新情報</h1>
+    <div className="min-h-screen flex flex-col items-center bg-white text-black">
+      <main className="flex flex-col items-center w-full p-6">
+        <ArticleCard />
+      </main>
+    </div>
     </>
   );
 }
+
+export default Home
