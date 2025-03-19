8d3861b14a274de1f82c4346c0f275a0d122d87c
marumo333 <seito5026junior@icloud.com>
2025-03-19 19:18:27 +0900

    jsonファイルを追加　jsonファイルをでコピー済み

--- src/app/article/[id]/articleData.py
+++ src/app/article/[id]/articleData.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import json
+import copy
+from urllib.parse import urljoin
 url='https://www.ea.com/ja/games/apex-legends/apex-legends/news/dev-update-0225'
 # ウェブページの内容を取得
 response = requests.get(url)
@@ -15,9 +18,16 @@ print(f"ページのタイトル: {title}")
 #記事の全体を取得
 full_text = soup.get_text(strip=True)
 print("ページ全体のテキスト:", full_text)
+original_data={'full_text':full_text}
+cloned_data=copy.deepcopy('original_data')
 
+with open ('cloned_data.json','w',encoding='utf-8') as json_file:
+    json.dump(cloned_data,json_file,indent=4,ensure_ascii=False)
 # すべてのリンクを取得
 links = soup.find_all('a')
 for link in links:
     href = link.get('href')
     print(href)
+    if href:
+        full_url=urljoin(url,href)
+        print(full_url)
